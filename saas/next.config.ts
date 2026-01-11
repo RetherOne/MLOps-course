902477b420eb3e70d6d779c39f6f3cf191d581ca
Vladyslav Boiko <vladyslav.boiko@student.tuke.sk>
2026-01-11 19:51:47 +0100

    AWS + docker added

--- saas/next.config.ts
+++ saas/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  reactStrictMode: true,
+  output: 'export',  // This exports static HTML/JS files
+  images: {
+    unoptimized: true  // Required for static export
+  },
+  // reactCompiler: true,
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
